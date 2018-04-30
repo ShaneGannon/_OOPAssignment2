338e12af76965e3ee78d544c45ccb2360efd5de2
SGannon98 <31891822+SGannon98@users.noreply.github.com>
2018-04-30 17:29:17 +0000

    Enemy animation and movement

---
+++ OOPAssignment/Assets/Scripts/EnemyControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControl : MonoBehaviour
+{
+    //enemy speed
+    float speed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //set speed
+        speed = 2f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //get enemy current position
+        Vector2 position = transform.position;
+
+        //compute the new position
+        position = new Vector2(position.x, position.y - speed * Time.deltaTime);
+
+        //update enemy position
+        transform.position = position;
+
+        //bottom left point of screen - control var
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+
+        //if enemy exits on bottom of screen destroy enemy
+        if (transform.position.y < min.y)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
